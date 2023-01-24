7c0c6e066821df93bca16d240a2bfadc2f2afc6c
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-01-24 20:59:30 +0100

    01.24
    Better version, still not working as intended

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,15 @@
 const rating = document.querySelectorAll('.rating__box');
 const button = document.querySelector('.button');
+const section = document.querySelector('.section');
 let msg;
 
 rating.forEach((item) => {
     item.addEventListener('click', () => {
         let score = item.textContent;
+        let feedback = document.createElement('p');
         item.classList.toggle('rating__box--active');
         msg = `You selected ${score} out of 5`;
-        console.log(msg);
+        feedback.textContent = msg;
+        section.appendChild(feedback);
     })
 })
